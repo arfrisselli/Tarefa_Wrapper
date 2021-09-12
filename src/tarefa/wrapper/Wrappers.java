c33d27910a54ba94e48989ccb507c606049a46bc
Andrey R. Frisselli <73029037+arfrisselli@users.noreply.github.com>
2021-09-12 14:35:56 -0300

    Adicionando metodo Scanner

--- src/tarefa/wrapper/Wrappers.java
+++ src/tarefa/wrapper/Wrappers.java
@@ -1,15 +1,21 @@
 package tarefa.wrapper;
 
+import java.util.Scanner;
+
 public class Wrappers {
 
 	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
 		
-		int inteiro = 99;
-		System.out.println("Vari·vel primitiva = " + inteiro);
+		System.out.println("Informe um n√∫mero inteiro:");
+		int inteiro = scan.nextInt();
 		
+		System.out.println("Vari√°vel primitiva = " + inteiro);
+
 		Integer intWrap = Integer.valueOf(inteiro);
-		System.out.println("Vari·vel wrapper = " + intWrap);
-		
+		System.out.println("Vari√°vel wrapper = " + intWrap);
+
 		float inteiro2 = inteiro;
 		System.out.println("Casting = " + inteiro2);
 
